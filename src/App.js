53feb2e0117110b3d2a4b94533e14d136fbc5dcb
Spencerp34 <spencerp34@yahoo.com>
2021-05-21 12:13:33 -0600

    did interview questions and set up basic site with some cringy css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
-import React from "react";
+import React from 'react';
+import { Route, Link, Switch } from 'react-router-dom'
+import './App.css';
+import PizzaBuilder from './components/BuildingPizza'
 
 const App = () => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <div>
+      <header className='header'>
+        <div className='navBar'>
+          <nav>
+            <Link to='/'>Home</Link>
+            <Link to='/pizza'>Order</Link>
+          </nav>
+        </div>
+        <img src='https://i.pinimg.com/originals/4c/5d/4c/4c5d4c3d3a303be1020e8374751ec549.jpg' alt='Pizza Planet' width='50%' id='PPlogo' ></img>
+      </header> 
+
+      <Switch>
+        <Route path="/pizza/:pizzaID">
+          <h2>Testing Pizza ID</h2>
+          {/* <Item/> */}
+        </Route>
+        <Route path='/pizza' render={props => {
+          return (
+            <PizzaBuilder />
+          )
+          
+          
+          // <ItemsList
+          //   // {...props}
+          //   items={stock} />
+        }} />
+        <Route path='/' render={props => {
+          return (
+            <>
+              <h2>Finest pizza in the Galaxy!</h2>
+              <h2>Order now!</h2>
+            </>
+          )
+        }}
+        
+        />
+      </Switch>
+
+    </div>
   );
 };
 export default App;
---
+++ src/components/BuildingPizza.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function BuildingPizza (props) {
+    
+    return (
+        <>
+            <h2>Testing Pizza Builder</h2>
+        </>
+    )
+}
